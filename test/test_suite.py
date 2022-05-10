3d3219480b3b5ecbef27ffbdff3a111daaad2036
smavros <stemavros@gmail.com>
2022-05-10 17:30:44 +0200

    Skip error tests

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,35 +1,89 @@
 import pytest
 from sqlalchemy.testing.suite import *
-from sqlalchemy.testing.suite.test_ddl import FutureTableDDLTest, TableDDLTest
-from sqlalchemy.testing.suite.test_dialect import DifficultParametersTest
-from sqlalchemy.testing.suite.test_select import FetchLimitOffsetTest, IsOrIsNotDistinctFromTest
+
+# Failures
 
 class DifficultParametersTest(DifficultParametersTest):
-    @pytest.mark.skip(reason="Monet does not support identifiers with an '%' in them.")
+    @pytest.mark.skip(reason="Monet does not support identifiers with "
+                      "an '%' in them.")
     def test_round_trip(*args, **kwargs):
         pass
 
+
 class IsOrIsNotDistinctFromTest(IsOrIsNotDistinctFromTest):
-    @pytest.mark.skip(reason="Monet does not support the 'IS DISTINCT FROM' syntax.")
+    @pytest.mark.skip(reason="Monet does not support the 'IS DISTINCT "
+                      "FROM' syntax.")
     def test_is_or_is_not_distinct_from():
         pass
+
 
 class TableDDLTest(TableDDLTest):
     @pytest.mark.skip(reason="column names of level >= 3")
     def test_create_table_schema(*args, **kwargs):
         pass
 
+
 class FutureTableDDLTest(FutureTableDDLTest):
     @pytest.mark.skip(reason="column names of level >= 3")
     def test_create_table_schema(*args, **kwargs):
         pass
 
+
 class FetchLimitOffsetTest(FetchLimitOffsetTest):
-    @pytest.mark.skip(reason="This test does some LIMIT statements in the middle of the query, this is not supported.")
+    @pytest.mark.skip(reason="This test does some LIMIT statements in "
+                      "the middle of the query, this is not supported.")
     def test_limit_render_multiple_times(*args, **kwargs):
         """
         This test does some LIMIT statements in the middle of the query,
         this is not supported.
         """
         pass
+
+
+# Errors
+
+class IdentityColumnTest(IdentityColumnTest):
+
+    @pytest.mark.skip(reason="This test names a table column 'desc' "
+                      "which collides with the SQL Standard DESC keyword")
+    def test_insert_always_error(self, connection):
+        pass
+
+    @pytest.mark.skip(reason="This test names a table column 'desc' "
+                      "which collides with the SQL Standard DESC keyword")
+    def test_select_all(self, connection):
+        pass
+
+    @pytest.mark.skip(reason="This test names a table column 'desc' "
+                      "which collides with the SQL Standard DESC keyword")
+    def test_select_columns(self, connection):
+        pass
+
+
+class IdentityAutoincrementTest(IdentityAutoincrementTest):
+
+    @pytest.mark.skip(reason="This test names a table column 'desc' "
+                      "which collides with the SQL Standard DESC keyword")
+    def test_autoincrement_with_identity(self, connection):
+        pass
+
+
+@pytest.mark.skip(reason="The tests of this class use self-reference "
+                  "foreign keys which are NOT supported by MonetDB")
+class CTETest(CTETest):
+    pass
+
+
+@pytest.mark.skip(reason="The tests of this class use the CHECK column "
+                  "constraint which is not support by MonetDB. The "
+                  "expression compiler gives a Warning so the user is "
+                  "notified for the skipped constraint but Warning are "
+                  "turned to exceptions from the test suite")
+class QuotedNameArgumentTest(QuotedNameArgumentTest):
+    pass
+
+
+@pytest.mark.skip(reason="The dialect is not supporting JSON type")
+class JSONTest(JSONTest):
+    pass
 
