7479012ef5f2c5832472baf8a830f325aa0a56da
Niels Nes <niels@cwi.nl>
2023-08-16 09:44:02 +0200

    some more tests

--- test/test_suite.py
+++ test/test_suite.py
@@ -2,19 +2,6 @@ import pytest
 from sqlalchemy.testing.suite import *
 
 # Failures
-
-class TableDDLTest(TableDDLTest):
-    @pytest.mark.skip(reason="column names of level >= 3")
-    def test_create_table_schema(*args, **kwargs):
-        pass
-
-
-class FutureTableDDLTest(FutureTableDDLTest):
-    @pytest.mark.skip(reason="column names of level >= 3")
-    def test_create_table_schema(*args, **kwargs):
-        pass
-
-
 class FetchLimitOffsetTest(FetchLimitOffsetTest):
     @pytest.mark.skip(reason="This test does some LIMIT statements in "
                       "the middle of the query, this is not supported.")
