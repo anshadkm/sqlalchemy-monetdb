24373165f9945d79e4cf64aa85f3d69850e235fa
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2023-08-29 15:37:27 +0300

    Fix package version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ readme = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup_params = dict(
     name="sqlalchemy_monetdb",
-    version='1.1.0',
+    version='2.0.0',
     description="SQLAlchemy dialect for MonetDB",
     author="Gijs Molenaar",
     author_email="gijsmolenaar@gmail.com",
